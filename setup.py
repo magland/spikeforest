83eb4b2b6ec603adcc83474170a6641a0808e755
Jeremy Magland <jeremy.magland@gmail.com>
2018-10-24 13:56:28 -0400

    fix test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name="spikeforest"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.1.1",
+    version="0.1.3",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="",
