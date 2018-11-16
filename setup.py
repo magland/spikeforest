d1b648717d3843c3b45d9dc1a070b62026e4c4b3
Jeremy Magland <jeremy.magland@gmail.com>
2018-11-16 15:02:39 -0500

    incr version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkg_name="spikeforest"
 
 setuptools.setup(
     name=pkg_name,
-    version="0.3.3",
+    version="0.3.4",
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="",
